a0c2bf44bac77c48b7d0074dbcb0897a634d3876
rrghiker <chris.ryan.hart1@gmail.com>
2023-01-06 17:24:33 -0500

    added dateTime format conversion to MeetupFormCard

--- climbing-meetup-frontend/src/MeetupFormCard.js
+++ climbing-meetup-frontend/src/MeetupFormCard.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 // import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import CountContext from './UserContext';
+import ClimbMeetupApi from './api';
 
 const useStyles = makeStyles({
   root: {
@@ -26,13 +29,44 @@ const useStyles = makeStyles({
 export default function MeetupFormCard() {
   const classes = useStyles();
 
-  const handleSubmit = (e) => {
+  const {currUserId} = useContext(CountContext);
 
-    // redirect to mymeetups after creation
+  const INITIAL_STATE = {
+    creatorUserId:currUserId,
+    date:'',
+    time:'',
+    duration:'',
+    location:'',
+    description:''
+  };
+
+  const [meetupFormData,setMeetupFormData] = useState(INITIAL_STATE);
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // convert date time to correct format
+    const {date, time} = meetupFormData;
+
+    const dateTime = date + 'T' + time;
+
+    // value to send to database
+    // value is converted to utc when submitted to database
+    const localDateTimeStr = new Date(dateTime);
+    
+    console.log('test');
+
+
+    let res = await ClimbMeetupApi.createMeetup(meetupFormData);
+    setMeetupFormData(INITIAL_STATE);
+
+    // redirect to users meetups
   }
 
   const handleChange = (e) => {
+    const {value, name} = e.target;
 
+    setMeetupFormData(data => ({...data, [name]: value}));
   }
 
   return (
@@ -46,29 +80,29 @@ export default function MeetupFormCard() {
             <form>
                 <div>
                     {/* <label>Creator User id: </label> */}
-                    <input name='creator_user_id'/>
+                    <input value={meetupFormData.creatorUserId} hidden name='creator_user_id'/>
                 </div>
                 <div>
-                    <label>Date: </label>
-                    <input/>  
+                    <label htmlFor='date'>Date: </label>
+                    <input onChange={handleChange} name='date' value={meetupFormData.date} type="date"/>  
                 </div>
                 <div>
-                    <label>Time: </label>
-                    <input/>  
+                    <label htmlFor='time'>Time: </label>
+                    <input onChange={handleChange} name='time' value={meetupFormData.time} type="time" step="900"/>  
                 </div>
                 <div>
-                    <label>Duration: </label>
-                    <input/>  
+                    <label htmlFor='duration'>Duration: </label>
+                    <input onChange={handleChange} name='duration' value={meetupFormData.duration} type="number"/>  
                 </div>
                 <div>
-                    <label>Location: </label>
-                    <input/>  
+                    <label htmlFor='location'>Location: </label>
+                    <input onChange={handleChange} name='location' value={meetupFormData.location} type="number"/>  
                 </div>
                 <div>
-                    <label>Description: </label>
-                    <textarea></textarea>  
+                    <label htmlFor='description'>Description: </label>
+                    <textarea onChange={handleChange} name='description' value={meetupFormData.description}></textarea>  
                 </div>
-                <button>Submit</button>
+                <Button variant='contained' onClick={handleSubmit}>Submit</Button>
             </form>
         </Typography>
       </CardContent>
